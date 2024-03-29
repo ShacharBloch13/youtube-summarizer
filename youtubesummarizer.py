7ecb86f41b8cf2e9248f9d2955f3fd4af4e15527
shachar.bloch <shachar.bloch@post.runi.ac.il>
2024-03-30 00:08:33 +0300

    Downloading top video

--- youtubesummarizer.py
+++ youtubesummarizer.py
@@ -1,28 +1,43 @@
 from googleapiclient.discovery import build
-import pytube
+from pytube import YouTube
+import isodate 
 import os
 
 api_key = 'AIzaSyCOevHFAmoNY3WONd-wzIoMPfGqA3ix4t0' # it is better practice to save locally under variables, but for the checkers to see that it is working, I will leave it here
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-def search_youtube(subject):
-    request = youtube.search().list(
-        part="snippet",
-        maxResults=5,
-        q=subject,
-        type="video"
-    )
-    response = request.execute()
+def search_and_download(subject):
+    search_request = youtube.search().list(
+    part="snippet",
+    maxResults=10,
+    q=subject,
+    type="video",
+    order="relevance",  # This ensures results are ordered by relevance
+)
+    search_response = search_request.execute()
 
-    for item in response['items']:
-        title = item['snippet']['title']
-        description = item['snippet']['description']
+    for item in search_response['items']:
         video_id = item['id']['videoId']
-        print(f"Title: {title}\nDescription: {description}\nVideo ID: {video_id}\n")
+
+        # Get video details to check the duration
+        video_request = youtube.videos().list(
+            part="contentDetails",
+            id=video_id
+        )
+        video_response = video_request.execute()
+
+        duration = isodate.parse_duration(video_response['items'][0]['contentDetails']['duration'])
+        if duration.total_seconds() < 600:  # Less than 10 minutes
+            print(f"Downloading video: {item['snippet']['title']} (ID: {video_id})")
+            YouTube(f'https://www.youtube.com/watch?v={video_id}').streams.first().download()
+            print("Download completed.")
+            return
+
+    print("No suitable video found.")
 
 def main():
     subject = input("Please enter a subject for the video: ")
-    search_youtube(subject)
+    search_and_download(subject)
 
 if __name__ == "__main__":
     main()
